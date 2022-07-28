03c719d2b05b1b7739c75fee8aebff1e0d98401a
NaweGa <104440972+NaweGa@users.noreply.github.com>
2022-07-28 20:17:53 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <section class="self-center flex flex-col flex-1 items-center justify-center">
       <h1 class="title text-center">NaweGa Status Blog</h1>
       <h2 class="subtitle text-center">NaweGa Server Status Report Blog</h2>
+      <h3 class="subtitle text-center">When server is down, system change server of nawe.ga to this page. Server isn't down but you want to see this report blog, visit <a href="https://report.nawe.ga">report.nawe.ga</a>.</h3>
     </section>
 
     <section class="mt-8">
